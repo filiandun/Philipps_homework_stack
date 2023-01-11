0d99258e4ed33c5fb896d014e7778d7bda33d9c1
filian <filiandun@gmail.com>
2023-01-11 22:35:25 +0300

    main: 99% ready

--- stack/main.cpp
+++ stack/main.cpp
@@ -1,7 +1,25 @@
 ﻿#include <iostream>
+#include "stack.h"
 
 int main()
 {
     setlocale(LC_ALL, "rus");
+
+    Stack stack;
+    // stack.delete_node(); // ничего удалятся не будет, так как стек пустой
+
+    stack.push_back(6);
+    stack.push_back(1);
+    stack.push_back(80);
+    stack.push_back(23);
+
+    stack.print();
+
+
+    std::cout << std::endl;
+    stack.delete_node();
+    stack.delete_node();
+
+    stack.print();
 
 }
