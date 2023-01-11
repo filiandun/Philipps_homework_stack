390dc58459628eb208af733089fa3935953132e2
filian <filiandun@gmail.com>
2023-01-11 22:32:42 +0300

    class Stack: 99% ready

--- stack/stack.cpp
+++ stack/stack.cpp
@@ -1,1 +1,56 @@
 #include "stack.h"
+
+Node::Node(unsigned long long int num)
+{
+	this->data = num;
+	this->previous = nullptr;
+}
+
+Stack::Stack() 
+{
+	this->end = nullptr;
+}
+
+Stack::~Stack()
+{
+	while (this->end != nullptr)
+	{
+		delete_node();
+	}
+}
+
+void Stack::push_back(unsigned long long int num)
+{
+	Node* new_node = new Node(num);
+
+	if (this->end != nullptr)
+	{
+		new_node->previous = this->end;
+	}
+	this->end = new_node;
+}
+
+void Stack::delete_node()
+{
+	if (this->end == nullptr)
+	{
+		std::cerr << "Ошибка: стек пустой!";
+		return;
+	}
+
+	Node* temp = this->end->previous;
+	delete this->end;
+	this->end = temp;
+}
+
+void Stack::print()
+{
+	Node* temp = this->end;
+
+	while (temp != nullptr)
+	{
+		std::cout << temp->data << " ";
+		temp = temp->previous;
+	}
+}
+
--- stack/stack.h
+++ stack/stack.h
@@ -1,7 +1,32 @@
 #pragma once
+#include <iostream>
+
+class Node
+{
+public:
+	Node() = delete;
+	Node(unsigned long long int num);
+
+public:
+	unsigned long long int data;
+	Node* previous;
+};
 
 class Stack
 {
+public:
+	Stack();
+	~Stack();
+
+private:
+	Node* end;
+
+public:
+	void push_back(unsigned long long int num);
+	void delete_node();
+	void print();
+
+
 
 };
 
